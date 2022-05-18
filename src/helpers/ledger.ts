7d5ffee2187e7ba97d53e7158434f7c3ad246edf
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-05-18 09:50:45 -0700

    use query block limit from constants

--- src/helpers/ledger.ts
+++ src/helpers/ledger.ts
@@ -3,9 +3,9 @@ import {
   SCERC721Derivative__factory,
   SealCredLedger,
 } from '@big-whale-labs/seal-cred-ledger-contract'
+import { QUERY_BLOCK_LIMIT } from '@big-whale-labs/constants'
 import Ledger from 'models/Ledger'
 import defaultProvider from 'helpers/defaultProvider'
-import queryBlockLimit from 'helpers/queryBlockLimit'
 
 export async function getLedgerRecord(
   sealCredLedger: SealCredLedger,
@@ -24,7 +24,10 @@ export async function getLedgerRecord(
 
 export default async function getLedger(sealCredLedger: SealCredLedger) {
   const eventsFilter = sealCredLedger.filters.SetMerkleRoot()
-  const events = await sealCredLedger.queryFilter(eventsFilter, queryBlockLimit)
+  const events = await sealCredLedger.queryFilter(
+    eventsFilter,
+    QUERY_BLOCK_LIMIT
+  )
   const ledger = {} as Ledger
   for (const event of events) {
     const { tokenAddress } = event.args
