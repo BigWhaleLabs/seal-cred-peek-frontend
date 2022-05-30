d6ea803ce775a969724f03fc08ff615ff60d9f40
Michel Erokhov <mixaileroxov@gmail.com>
2022-05-30 11:57:32 +0300

    install @big-whale-labs/frontend-utils

--- src/helpers/getLedger.ts
+++ src/helpers/getLedger.ts
@@ -1,32 +1,18 @@
 import { SealCredLedger } from '@big-whale-labs/seal-cred-ledger-contract'
+import { getAddressesToMerkleRoot } from '@big-whale-labs/frontend-utils'
 import Ledger from 'models/Ledger'
-import getAllEvents from 'helpers/getAllEvents'
 import getLedgerRecord from 'helpers/getLedgerRecord'
 
 export default async function (sealCredLedger: SealCredLedger) {
-  const { events, deleteTopic } = await getAllEvents(sealCredLedger)
+  const addressToMerkleRoot = await getAddressesToMerkleRoot(sealCredLedger)
 
   const ledger = {} as Ledger
-  const addressToMerkle: { [address: string]: string } = {}
 
-  for (const event of events) {
-    const {
-      args: { tokenAddress, merkleRoot },
-      topic,
-    } = event
-
-    if (topic === deleteTopic) {
-      delete addressToMerkle[tokenAddress]
-      continue
-    }
-    addressToMerkle[tokenAddress] = merkleRoot
-  }
-
-  for (const tokenAddress in addressToMerkle) {
+  for (const tokenAddress in addressToMerkleRoot) {
     ledger[tokenAddress] = await getLedgerRecord(
       sealCredLedger,
       tokenAddress,
-      addressToMerkle[tokenAddress]
+      addressToMerkleRoot[tokenAddress]
     )
   }
   return ledger
