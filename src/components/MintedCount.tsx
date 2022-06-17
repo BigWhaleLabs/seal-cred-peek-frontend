0bfbc1a6bde6cc79986c8bb61e7592075ae55d1e
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-17 07:30:10 -0700

    comment out the rest

--- src/components/MintedCount.tsx
+++ src/components/MintedCount.tsx
@@ -3,22 +3,22 @@ import { Suspense } from 'react'
 import { useSnapshot } from 'valtio'
 import Loading from 'components/Loading'
 import SealCredStore from 'stores/SealCredStore'
-import StatsStore from 'stores/StatsStore'
+// import StatsStore from 'stores/StatsStore'
 
 function MintedCount() {
   const { ledger } = useSnapshot(SealCredStore)
-  const { originalCount, derivativeCount } = useSnapshot(StatsStore)
+  // const { originalCount, derivativeCount } = useSnapshot(StatsStore)
 
   return (
     <>
       <BodyText>Total contracts: {Object.keys(ledger).length}</BodyText>
       <BodyText>
         Total minted original:{' '}
-        {Object.values(originalCount).reduce((sum, count) => sum + count, 0)}
+        {/* {Object.values(originalCount).reduce((sum, count) => sum + count, 0)} */}
       </BodyText>
       <BodyText>
         Total minted derivative:{' '}
-        {Object.values(derivativeCount).reduce((sum, count) => sum + count, 0)}
+        {/* {Object.values(derivativeCount).reduce((sum, count) => sum + count, 0)} */}
       </BodyText>
     </>
   )
--- src/stores/StatsStore.ts
+++ src/stores/StatsStore.ts
@@ -1,31 +1,25 @@
 import { derive } from 'valtio/utils'
-import SealCredStore from 'stores/SealCredStore'
+// import SealCredStore from 'stores/SealCredStore'
 
 export default derive({
-  originalCount: async (get) => {
-    const state = get(SealCredStore)
-
-    const original: { [address: string]: number } = {}
-
-    for (const address in state.originalContractsToOwnersMaps) {
-      original[address] = Object.keys(
-        await state.originalContractsToOwnersMaps[address]
-      ).length
-    }
-
-    return original
-  },
-  derivativeCount: async (get) => {
-    const state = get(SealCredStore)
-
-    const derivative: { [address: string]: number } = {}
-
-    for (const address in state.derivativeContractsToOwnersMaps) {
-      derivative[address] = Object.keys(
-        await state.derivativeContractsToOwnersMaps[address]
-      ).length
-    }
-
-    return derivative
-  },
+  // originalCount: async (get) => {
+  //   const state = get(SealCredStore)
+  //   const original: { [address: string]: number } = {}
+  //   for (const address in state.originalContractsToOwnersMaps) {
+  //     original[address] = Object.keys(
+  //       await state.originalContractsToOwnersMaps[address]
+  //     ).length
+  //   }
+  //   return original
+  // },
+  // derivativeCount: async (get) => {
+  //   const state = get(SealCredStore)
+  //   const derivative: { [address: string]: number } = {}
+  //   for (const address in state.derivativeContractsToOwnersMaps) {
+  //     derivative[address] = Object.keys(
+  //       await state.derivativeContractsToOwnersMaps[address]
+  //     ).length
+  //   }
+  //   return derivative
+  // },
 })
