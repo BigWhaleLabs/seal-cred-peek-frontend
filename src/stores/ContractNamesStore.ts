1db679cf11c45077e22b683187e4508057085068
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-09 16:13:32 -0700

    up dependencies and fix store integration

--- src/stores/ContractNamesStore.ts
+++ src/stores/ContractNamesStore.ts
@@ -1,4 +1,4 @@
 import { ContractNamesStore } from '@big-whale-labs/stores'
 import { proxy } from 'valtio'
 
-export default proxy(new ContractNamesStore()).makePersistent(true)
+export default proxy(new ContractNamesStore()).makePersistent()
