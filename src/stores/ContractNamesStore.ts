732896f53923719636ff69ba6d8cad2be70c178d
Ruslan Zapevalov <upacyxou@tutanota.com>
2022-08-09 22:14:16 +0400

    fix checks

--- src/stores/ContractNamesStore.ts
+++ src/stores/ContractNamesStore.ts
@@ -1,10 +1,5 @@
-import { ContractNamesStore } from '@big-whale-labs/store-utils'
-import { ERC721__factory } from '@big-whale-labs/seal-cred-ledger-contract'
-import { RESERVED_CONTRACT_METADATA } from '@big-whale-labs/constants'
-import { goerliProvider, mainnetProvider } from 'helpers/providers'
+import { ContractNamesStore } from '@big-whale-labs/stores'
 import { proxy } from 'valtio'
-import Network from 'models/Network'
-import networkPick from 'helpers/networkPick'
 
 export default proxy(
   new ContractNamesStore(['requestedNames', 'contractNames'])
