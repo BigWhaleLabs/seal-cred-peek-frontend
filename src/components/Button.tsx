4fa73941df22984d549900ebfb5184ef7bc57391
smyzin <smyzin.work@gmail.com>
2023-05-06 13:50:34 +0300

    Removed cookie code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,96 +1,14 @@
-import {
-  alignItems,
-  backgroundColor,
-  borderRadius,
-  boxShadow,
-  boxShadowColor,
-  brightness,
-  classnames,
-  display,
-  fontSize,
-  fontWeight,
-  gap,
-  justifyContent,
-  lineHeight,
-  padding,
-  textDecoration,
-  transitionDuration,
-  transitionProperty,
-  transitionTimingFunction,
-} from 'classnames/tailwind'
+import { textDecoration } from 'classnames/tailwind'
 import ChildrenProp from 'models/ChildrenProp'
 
-const commonClasses = ({
-  small,
-  type,
-}: {
-  type: ButtonType
-  small?: boolean
-}) => {
-  if (type === 'transparent') return textDecoration('underline')
-
-  return classnames(
-    display('flex'),
-    gap('gap-x-2'),
-    justifyContent('justify-center'),
-    alignItems('items-center'),
-    fontWeight('font-bold'),
-    transitionProperty('transition-all'),
-    transitionTimingFunction('ease-in-out'),
-    transitionDuration('duration-100'),
-    boxShadow('active:shadow-button-active', 'hover:shadow-lg', 'shadow-2xl'),
-    fontSize(small ? 'text-sm' : 'text-lg'),
-    lineHeight(small ? 'leading-5' : 'leading-7'),
-    padding(
-      small ? { 'px-4': true, 'py-2': true } : { 'px-6': true, 'py-4': true }
-    )
-  )
-}
-
-const button = (type: ButtonType, small?: boolean) =>
-  classnames(
-    commonClasses({
-      small,
-      type,
-    }),
-    colorClasses(type)
-  )
-
-const colorClasses = (type: ButtonType) => {
-  switch (type) {
-    case 'primary':
-      return classnames(
-        borderRadius('rounded-full'),
-        backgroundColor('bg-tertiary'),
-        boxShadowColor(
-          'shadow-tertiary',
-          'hover:shadow-tertiary',
-          'active:shadow-tertiary'
-        ),
-        boxShadow('shadow-button'),
-        brightness('active:brightness-90', 'hover:brightness-95')
-      )
-    default:
-      return backgroundColor('bg-transparent')
-  }
-}
-
-interface ButtonProps {
-  type?: ButtonType
-  small?: boolean
-  onClick: () => void
-}
-
-type ButtonType = 'primary' | 'transparent'
+const button = textDecoration('underline')
 
 export default function ({
   children,
-  small,
-  type = 'transparent',
   onClick,
-}: ButtonProps & ChildrenProp) {
+}: ChildrenProp & { onClick: () => void }) {
   return (
-    <button className={button(type, small)} onClick={onClick}>
+    <button className={button} onClick={onClick}>
       {children}
     </button>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'formal-accent': 'var(--formal-accent)',
-        tertiary: 'var(--tertiary)',
-        primary: 'var(--primary)',
-        'primary-dark': 'var(--primary-dark)',
-        'primary-semi-transparent': 'var(--primary-semi-transparent)',
         black: {
           background: 'var(--black-background)',
         },
-      },
-      boxShadow: {
-        '2xl': '0rem 0.25rem 2.75rem 0rem rgb(0 0 0 / 0.25)',
-        lg: '0rem 0rem 1rem 0rem rgb(0 0 0 / 0.25)',
-        button: '0rem 0rem 1.625rem rgb(0 0 0 / 1)',
-        'button-active': '0rem 0rem 0.375rem rgb(0 0 0 / 1)',
-        card: '0rem 0.25rem 2.75rem 0rem rgb(0 0 0 / 0.25), inset 0px 0px 1rem rgb(0 0 0 / 0.25)',
-      },
-      maxWidth: {
-        cookie: '25.625rem',
       },
     },
     container: {
